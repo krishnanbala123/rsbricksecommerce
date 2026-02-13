9916e9a1fe7cd0414f842e0a2c3c2993a4ba309a
krishnanbala123 <krishnanbala7183@gmail.com>
2026-02-13 12:15:22 +0530

    post login update

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -2,37 +2,7 @@
 // import connectDB from "@/lib/mongodb";
 // import User from "@/models/User";
 
-// export async function POST(req) {
-//   try {
-//     await connectDB();
 
-//     const body = await req.json();
-
-//     console.log("📥 API USERS BODY:", body);
-
-//     const user = await User.findOneAndUpdate(
-//       { uid: body.uid },
-//       {
-//         name: body.name || "",
-//         email: body.email || "",
-//         phone: body.phone || "",
-//         photoURL: body.photoURL || "",
-//         lastLogin: new Date(),
-//       },
-//       { upsert: true, new: true }
-//     );
-
-//     console.log("✅ USER SAVED:", user);
-
-//     return NextResponse.json({ success: true, user });
-//   } catch (err) {
-//     console.error("❌ USERS API ERROR:", err);
-//     return NextResponse.json(
-//       { success: false, error: "User save failed" },
-//       { status: 500 }
-//     );
-//   }
-// }
 
 
 
@@ -47,6 +17,38 @@ export async function GET(req) {
 
   const user = await User.findOne({ uid });
   return NextResponse.json({ user });
+}
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+
+    console.log("📥 API USERS BODY:", body);
+
+    const user = await User.findOneAndUpdate(
+      { uid: body.uid },
+      {
+        name: body.name || "",
+        email: body.email || "",
+        phone: body.phone || "",
+        photoURL: body.photoURL || "",
+        lastLogin: new Date(),
+      },
+      { upsert: true, new: true }
+    );
+
+    console.log("✅ USER SAVED:", user);
+
+    return NextResponse.json({ success: true, user });
+  } catch (err) {
+    console.error("❌ USERS API ERROR:", err);
+    return NextResponse.json(
+      { success: false, error: "User save failed" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function PUT(req) {
